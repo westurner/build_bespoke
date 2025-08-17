f2a525376765ccbe06e5455e9f48b1122da7b29a
@westurner <wes@wrd.nu>
2025-08-17 17:53:44 -0400

    BLD: Makefile,build_bespoke.sh: PODMAN_BUILD_USER, BS_REPO_URL, BS_REPO_BRANCH; and add arg handling in main() for GIT_REPO_URL GIT_REPO_BRANCH

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ PODMAN_ROOTLESS_OPTS=--security-opt=label=disable --userns=keep-id
 PODMAN_OPTS=-e HOME=/workspace
 PODMAN_RM=--rm
 PODMAN_VOLUMES=-v "${HOME}/-wrk/-ve311/arts:/workspace" -v "${HOME}/-wrk/arts:${HOME}/-wrk/arts"
-
+PODMAN_ARGS=
 
 podman-build:
 	${PODMAN} build -t westurner/bespokesynthsrc -f "Dockerfile.bespoke"
@@ -26,10 +26,21 @@ podman-run:
 		${PODMAN_ROOTLESS_OPTS} \
 		${PODMAN_VOLUMES} \
 		${PODMAN_OPTS} \
+		${PODMAN_ARGS} \
 		${PODMAN_RM} \
 		-it westurner/bespokesynthsrc \
 		${PODMAN_CMD}
 
-
 podman-run-run:
 	$(MAKE) podman-run PODMAN_CMD="/BespokeSynth/ignore/build/Source/BespokeSynth_artefacts/Release/BespokeSynth"
+
+
+PODMAN_BUILD_USER=appuser
+
+
+BS_REPO_URL=https://github.com/westurner/bespokesynth
+BS_REPO_BRANCH=main
+
+podman-run-buildbespoke:
+	$(MAKE) podman-run PODMAN_ARGS='--user=root' PODMAN_CMD="sh -c 'sudo -u '$(PODMAN_BUILD_USER)' GIT_REPO_URL='$(BS_REPO_URL)' GIT_REPO_BRANCH='$(BS_REPO_BRANCH)' sh ./build_bespoke.sh -v all'"
+
--- build_bespoke.sh
+++ build_bespoke.sh
@@ -19,6 +19,7 @@ export DO_GIT_CLONE=${DO_GIT_CLONE}
 export SOURCE_URL="https://github.com/BespokeSynth/BespokeSynth"
 export SOURCE_URL_westurner="https://github.com/westurner/BespokeSynth"
 
+
 update_grubby_and_rpmostree() {
     type -a grubby && sudo grubby --args="preempt=full" --update-kernel=ALL
     type -a rpm-ostree && sudo rpm-ostree kargs --append=
@@ -26,19 +27,9 @@ update_grubby_and_rpmostree() {
 }
 
 
-if [ "$(id -u)" -eq 0 ]; then
-    sudo dnf install -y cmake clang python3-devel alsa-lib-devel freetype-devel mesa-libGL-devel libcurl-devel webkit2gtk4.0-devel gtk+-devel pipewire-jack-audio-connection-kit-devel
-else
-    echo "Skipping dnf install because running as non-root"
-fi
-
-
-if [ ! -d "BespokeSynth" ] && [ ! -z "${DO_GIT_CLONE}" ]; then
-    git clone https://github.com/BespokeSynth/BespokeSynth && \
-        cd BespokeSynth && \
-        git submodule update --init --recursive
-fi
-
+install_packages() {
+    sudo dnf install -y cmake clang python3-devel alsa-lib-devel freetype-devel mesa-libGL-devel libcurl-devel webkit2gtk4.0-devel gtk+-devel pipewire-jack-audio-connection-kit-devel libusb1-devel
+}
 
 _mv_cmake_cache_file() {
     local cmakecache=$1
@@ -47,10 +38,118 @@ _mv_cmake_cache_file() {
         mv "${cmakecache}" "${cmakecache}.bkp-$(date -Is).txt"
 }
 
-_mv_cmake_cache_file "BespokeSynth/ignore/build/CMakeCache.txt"
-_mv_cmake_cache_file "BespokeSynth/ignore/build/JUCE/tools/CMakeCache.txt"
+__THIS=$0
+print_usage() {
+    echo "${__THIS} -- Build Bespokesynth"
+    echo ""
+    echo " -h,--help,help   Print help"
+    echo " -v,--verbose     set -x -v"
+    echo ""
+    echo " --install, install    Install packages necessary to build"
+    echo " --install-only        Only install packages necessary to build and then exit"
+    echo " --install-kernel-args update kernel args (with grubby and rpmostree)"
+    echo " --clone, clone        Clone BespokeSynth"
+    echo " --mv-cmake-cache      mv cmake build cache dirs   " # TODO: clean
+    echo " --build, build        Build BespokeSynth"
+    echo " all, --all            Run all tasks"
+    echo ""
+    echo " --allow-build-as-root Run git clone and make build as root"
+}
+
+main() {
+    if [ -z "${*}" ]; then
+        print_usage
+    fi
+    for arg in "${@}"; do
+        case "$arg" in
+            -h|--help|help)
+                print_usage
+                exit 0
+                ;;
+            -v|--verbose)
+                set -x -v
+                ;;
+            --install|install)
+                export DO_INSTALL=1
+                ;;
+            --install-kernel-args|install-kernel-args|install-kargs)
+                export DO_INSTALL_KERNEL_ARGS=1
+                ;;
+            --install-only|install-only)
+                export DO_INSTALL_ONLY=1
+                ;;
+            --clone|clone)
+                export DO_GIT_CLONE=1
+                ;;
+            --mv-cmake-cache)
+                export DO_MV_CMAKE_CACHE_FILE=1
+                ;;
+            --build|build)
+                export DO_BUILD=1
+                ;;
+            --allow-build-as-root)
+                export DO_ALL_AS_ROOT=1
+                ;;
+            all|--all)
+                export DO_ALL=1
+                export DO_INSTALL_KERNEL_ARGS=1
+                export DO_INSTALL=1
+                export DO_INSTALL_ONLY=0
+                export DO_GIT_CLONE=1
+                export DO_MV_CMAKE_CACHE_FILE=1
+                export DO_BUILD=1
+                ;;
+        esac
+    done
+
+    export GIT_REPO_URL="${GIT_REPO_URL:-"https://github.com/BespokeSynth/BespokeSynth"}"
+    export GIT_REPO_BRANCH="${GIT_REPO_BRANCH:-""}"
 
 
-cd BespokeSynth/
-cmake -Bignore/build -DCMAKE_BUILD_TYPE=Release
-cmake --build ignore/build --parallel 4 --config Release
+    if [ "$(id -u)" -eq 0 ]; then
+        if [ -n "${DO_ALL}" ]; then
+            if [ -z "${DO_ALL_AS_ROOT}" ]; then
+                echo "INFO: --allow-build-as-root was not specified. Not building as root."
+                export DO_INSTALL_KERNEL_ARGS=1
+                export DO_INSTALL_ONLY=1
+            else
+                echo "INFO: DO_ALL_AS_ROOT=1"
+            fi
+        fi
+    fi
+
+    if [ "$(id -u)" -eq 0 ]; then
+        if [ -n "${DO_INSTALL_KERNEL_ARGS}" ]; then
+            echo "INFO: update kernel args with grubby and rpmostree"
+            update_grubby_and_rpmostree
+        fi
+
+        if  [ -n "${DO_INSTALL_ONLY}" ] || [ -n "${DO_INSTALL}" ]; then
+            echo "INFO: update kernel args with grubby and rpmostree"
+            install_packages
+        fi
+    fi
+
+    if [ -n "${DO_GIT_CLONE}" ] && [ ! -d "bespokesynth/" ] ; then
+        (git clone "${GIT_REPO_URL}" ${GIT_REPO_BRANCH:+"-b"} "${GIT_REPO_BRANCH:+"${GIT_REPO_BRANCH}"}" && \
+            cd bespokesynth/ && \
+            git submodule update --init --recursive)
+    fi
+
+
+    if [ -n "${DO_BUILD}" ] || [ -n "${DO_MV_CMAKE_CACHE_FILE}" ]; then
+        (cd bespokesynth/;
+        _mv_cmake_cache_file "BespokeSynth/ignore/build/CMakeCache.txt"; 
+        _mv_cmake_cache_file "BespokeSynth/ignore/build/JUCE/tools/CMakeCache.txt";)
+    fi
+
+  
+
+    if [ -n "${DO_BUILD}" ];  then
+        (cd bespokesynth/;
+        cmake -Bignore/build -DCMAKE_BUILD_TYPE=Release;
+        cmake --build ignore/build --parallel 4 --config Release;)
+    fi
+}
+
+main "${@}"
