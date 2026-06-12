2810bc84a5faab1ba5bfb1fef3c55ab5b97308dc
@westurner <wes@wrd.nu>
2026-06-12 16:05:37 +0000

    CLN: remove tests (added to BespokeSynth)

--- tests/JuceLibraryCode.cpp
+++
@@ -1,4 +0,0 @@
-
-#define JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED 1
-#include <juce_core/juce_core.cpp>
-#include <juce_events/juce_events.cpp>
--- tests/TestRunner.cpp
+++
@@ -1,8 +0,0 @@
-#include <juce_core/juce_core.h>
-
-int main(int argc, char* argv[])
-{
-    juce::UnitTestRunner runner;
-    runner.runAllTests();
-    return 0;
-}
